7f7071937df3a130d45ae277dcaaf44318f731e2
noodleWrecker7 <hodgkinson.adam@gmail.com>
2021-04-21 12:50:49 +0100

    Initial commit

---
+++ src/main/java/dev/adamhodgkinson/App.java
@@ -0,0 +1,12 @@
+package dev.adamhodgkinson;
+
+import javafx.stage.Stage;
+
+public class App extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setWidth(960);
+        stage.setHeight(540);
+        stage.show();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module Client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports dev.adamhodgkinson;
+}
